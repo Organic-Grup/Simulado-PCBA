e9072db0ac1a86d9b9c75630be47fcaccf08f4be
Orgânic/Grup <124021536+Organic-Grup@users.noreply.github.com>
2026-05-23 19:00:16 -0300

    Refactor cache handling in service worker

--- sw.js
+++ sw.js
@@ -15,16 +15,10 @@ self.addEventListener("install", event => {
     self.skipWaiting();
 
     event.waitUntil(
-
         caches.open(CACHE_NAME)
         .then(cache => {
-
-            return cache.addAll(
-                urlsToCache
-            );
-
+            return cache.addAll(urlsToCache);
         })
-
     );
 
 });
@@ -32,14 +26,10 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
 
     event.respondWith(
-
         caches.match(event.request)
         .then(response => {
-
             return response || fetch(event.request);
-
         })
-
     );
 
 });
@@ -53,11 +43,9 @@ self.addEventListener("activate", event => {
             caches.keys().then(keys => {
 
                 return Promise.all(
-
                     keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-
+                        .filter(key => key !== CACHE_NAME)
+                        .map(key => caches.delete(key))
                 );
 
             }),
